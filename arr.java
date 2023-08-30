fa8b3100c8e411eadf235894caeb787e67694bc5
Chetan Lambe <138874931+Chetanlambe@users.noreply.github.com>
2023-08-30 19:23:27 +0530

    print the array index number
    print the array index number

---
+++ arr.java
@@ -0,0 +1,20 @@
+package arrays;
+public class arr {
+    public static void main(String[] args) {
+        int a[] = {1, 5, 9, 7};
+
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+
+        // Upadation
+        a[2] = 24;
+        System.out.println(a[2]);
+
+        double b[] = {1.2, 2.2, 3.1 };
+        System.out.println(b[0]);
+        System.out.println(b[1]);
+        System.out.println(b[2]);
+    }
+}
