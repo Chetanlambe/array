e335a2b0c455874febafb65316d710aef17d3976
Chetan Lambe <138874931+Chetanlambe@users.noreply.github.com>
2023-08-30 19:47:27 +0530

    in array input taken from user
    arrray input taken from the user

---
+++ arr1.java
@@ -0,0 +1,21 @@
+package arrays;
+
+import java.util.Scanner;
+public class arr1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a[] = new int[3];
+        System.out.println("Enter the first element");
+        a[0] = sc.nextInt();
+        System.out.println("Enter the first element");
+        a[1] = sc.nextInt();
+        System.out.println("Enter the first element");
+        a[2] = sc.nextInt();
+
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+
+
+    }
+}
